7806f2163a1a404ec6a14a722f9f0e4a5a1f56e8
Niklas Hökenström <niklashokenstrom@gmail.com>
2014-03-12 12:03:37 +0100

    Problem set 3

---
+++ Erlang_Jonas_och_niklas/src/add.erl
@@ -0,0 +1,24 @@
+%% @doc Erlang mini project.
+-module(add).
+-export([start/3, start/4]).
+
+%% @doc TODO: add documentation
+-spec start(A,B,Base) -> ok when 
+      A::integer(),
+      B::integer(), 
+      Base::integer().
+
+start(A,B, Base) ->
+    io:format(integer_to_list(A+B,Base)).
+
+%% @doc TODO: add documentation
+-spec start(A,B,Base, Options) -> ok when 
+      A::integer(),
+      B::integer(), 
+      Base::integer(),
+      Option::atom() | tuple(),
+      Options::[Option].
+
+start(A,B,Base, Options) ->
+    tbi.
+
--- bst/bst test/large_test.c
+++ bst/bst test/large_test.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h> /* rand(), srand() */
+#include <time.h>   /* time() */
 //#include "bst.h"
 //#include "timing.h"
 #include "bst_test.h"
@@ -7,12 +10,16 @@
 int main()
 {
   int n = 10;
-  int nthreads = 1;
-  int threads_iterations = 16384;
+  int nthreads;// = 2;
+  int threads_iterations;// = 1000;
   int result;
   int succesful = 0; 
+  int seed = time(NULL);
+  srand(seed);
   for (int i = 1; i < n+1; i++) {
-    result = bst_test(nthreads*(pow(2*i, threads_iterations/(2*i));
+    nthreads = (int) pow(2.0, (double) (rand() % 6));
+    threads_iterations = (int) pow(100.0, (double) (rand() % 1000));
+    result = bst_test(nthreads, threads_iterations);
     succesful += result;
     if ( result == 1) {
       printf("Test %d sucessful!/n", i);
